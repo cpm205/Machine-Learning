6cfbe06e56c457100383da217cace647db1e7a1f
Derek Hu <yang.derek.hu@gmail.com>
2017-08-13 22:01:15 +1200

    if else

---
+++ Datacamp/R/syntax/ifelse.R
@@ -0,0 +1,12 @@
+medium <- "LinkedIn"
+num_views <- 14
+
+# Examine the if statement for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+}
+
+# Write the if statement for num_views
+if(num_views > 15){
+  print("You're popular!")
+}
--- Datacamp/R/syntax/operator.R
+++ Datacamp/R/syntax/operator.R
@@ -31,4 +31,13 @@ last < 5 | last > 10
 # Is last between 15 (exclusive) and 20 (inclusive)?
 last > 15 & last <= 20
 
+# linkedin exceeds 10 but facebook below 10
+linkedin > 10 & facebook < 10
+
+# When were one or both visited at least 12 times?
+linkedin >= 12 | facebook >= 12
+
+# When is views between 11 (exclusive) and 14 (inclusive)?
+views > 11 & views <= 14
+
 
