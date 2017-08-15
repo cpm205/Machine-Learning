e09ff217ff18fc3a4969d87ab47d80c34e5c2bfa
Derek Hu <yang.derek.hu@gmail.com>
2017-08-16 09:31:59 +1200

    function

--- Datacamp/R/syntax/function.R
+++ Datacamp/R/syntax/function.R
@@ -40,3 +40,19 @@ mean(linkedin)
 
 # Advanced average of linkedin
 mean(linkedin,na.rm = TRUE)
+
+# Create a function pow_two()
+pow_two <- function(arg){
+  arg * arg
+}
+
+# Use the function
+pow_two(12)
+
+# Create a function sum_abs()
+sum_abs <- function(arg1,arg2){
+  abs(arg1) + abs(arg2)
+}
+
+# Use the function
+sum_abs(-2,3)
