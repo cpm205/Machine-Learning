23e045a5516184630a8130966d1aa0e90d9048c3
derekh <derekh@datacom.co.nz>
2017-08-29 11:57:01 +1200

    add more comments

--- Datacamp/R/syntax/Matrix.R
+++ Datacamp/R/syntax/Matrix.R
@@ -1,4 +1,4 @@
-
+#Matrix and vector contains data which has same data-type
 #In the matrix() function:
 #The first argument is the collection of elements that R will arrange into the rows and columns of the matrix. Here, we use 1:9 which is a shortcut for c(1, 2, 3, 4, 5, 6, 7, 8, 9).
 #The argument byrow indicates that the matrix is filled by the rows. If we want the matrix to be filled by the columns, we just place byrow = FALSE.
