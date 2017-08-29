48387a6083db7b5465ccce34cb297e66113936b0
derekh <derekh@datacom.co.nz>
2017-08-29 18:17:06 +1200

    vapply

---
+++ Datacamp/R/syntax/vapply.R
@@ -0,0 +1,3 @@
+#lapply(): apply function over list or vector, output = list
+#sapply(): apply function over list or vector, try to simplify list to array
+#vapply(): apply function over list or vector, explicity specify output format which means you have to specify the return type
