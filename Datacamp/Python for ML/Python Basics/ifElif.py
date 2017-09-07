903ea43e525dce4d3d7d8db78ba178ae1a92dee9
derekh <derekh@datacom.co.nz>
2017-09-07 18:22:46 +1200

    Logic

---
+++ Datacamp/Python for ML/Python Basics/ifElif.py
@@ -0,0 +1,24 @@
+# Define variables
+room = "kit"
+area = 14.0
+
+# if-else construct for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+else :
+    print("looking around elsewhere.")
+
+# if-else construct for area
+if area > 15 :
+    print("big place!")
+else :
+    print("pretty small.")
+
+
+# if-elif-else construct for area
+if area > 15 :
+    print("big place!")
+elif area > 10:
+    print( "medium size, nice!")
+else :
+    print("pretty small.")
--- Datacamp/Python for ML/Python Basics/pandas2.py
+++ Datacamp/Python for ML/Python Basics/pandas2.py
@@ -7,6 +7,7 @@
 #3. Row&Column access brics.loc[["RU","IN","CH"],["country","capital"]] or brics.iloc[[1,2,3],[0,1]]
 
 import pandas as pd
+import numpy as np
 
 # Pre-defined lists
 names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
@@ -67,3 +68,28 @@ print(cars.iloc[:, [2]])
 # Print out cars_per_cap and drives_right as DataFrame
 print(cars.loc[:, ['cars_per_cap', 'drives_right']])
 print(cars.iloc[:, [0, 2]])
+
+
+#Filtering Pandas DataFrame
+# Extract drives_right column as Series: dr
+dr = cars.loc[:,"drives_right"]
+# Use dr to subset cars: sel
+sel = cars[dr]
+# Print sel
+print(sel)
+
+# Create car_maniac: observations that have a cars_per_cap over 500
+cpc = cars.loc[:,"cars_per_cap"]
+many_cars = cpc > 500
+car_maniac = cars[many_cars]
+# Print car_maniac
+print(car_maniac)
+
+
+# Create medium: observations with cars_per_cap between 100 and 500
+import numpy as np
+cpc = cars['cars_per_cap']
+between = np.logical_and(cpc > 100, cpc < 500)
+medium = cars[between]
+# Print medium
+print(medium)
