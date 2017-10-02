f657ffcff34e06e48f76ced81d091a9e43e1f86b
derekh <derekh@datacom.co.nz>
2017-10-03 11:42:48 +1300

    Data frames and lists can contain multiple data types.

--- Datacamp/R/syntax/Lists.R
+++ Datacamp/R/syntax/Lists.R
@@ -3,7 +3,7 @@
 #Data frames (two-dimensional objects): can hold numeric, character or logical values. Within a column all elements have the same data type, but different columns can be of different data type
 #A list in R allows you to gather a variety of objects under one name (that is, the name of the list) in an ordered way. These objects can be matrices, vectors, data frames, even other lists, etc. It is not even required that these objects are related to each other in any way.
 #You could say that a list is some kind super data type: you can store practically any piece of information in it!
-#Only Data frames and matrics can contain multiple data types.
+#Data frames and lists can contain multiple data types.
 
 # Vector with numerics from 1 up to 10
 my_vector <- 1:10 
