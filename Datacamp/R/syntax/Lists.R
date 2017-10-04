a644738c603f4f2d587df6a33bef2ff5d858b4eb
derekh <derekh@datacom.co.nz>
2017-10-05 12:18:39 +1300

    Which of the following data structures can contain elements of only one data type?Matrices and vectors

--- Datacamp/R/syntax/Lists.R
+++ Datacamp/R/syntax/Lists.R
@@ -4,6 +4,7 @@
 #A list in R allows you to gather a variety of objects under one name (that is, the name of the list) in an ordered way. These objects can be matrices, vectors, data frames, even other lists, etc. It is not even required that these objects are related to each other in any way.
 #You could say that a list is some kind super data type: you can store practically any piece of information in it!
 #Data frames and lists can contain multiple data types.
+#Which of the following data structures can contain elements of only one data type?Matrices and vectors
 
 # Vector with numerics from 1 up to 10
 my_vector <- 1:10 
