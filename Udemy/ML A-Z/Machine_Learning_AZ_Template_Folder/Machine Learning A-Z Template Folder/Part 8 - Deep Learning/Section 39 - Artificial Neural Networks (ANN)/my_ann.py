54781fc3fc0cb1764539febdd480f1cc2b5a3084
Derek Hu <derekh@datacom.co.nz>
2018-11-02 17:03:06 +1300

    Data pre-processing

--- Udemy/ML A-Z/Machine_Learning_AZ_Template_Folder/Machine Learning A-Z Template Folder/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/my_ann.py
+++ Udemy/ML A-Z/Machine_Learning_AZ_Template_Folder/Machine Learning A-Z Template Folder/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/my_ann.py
@@ -4,4 +4,46 @@ Created on Mon Oct 29 23:28:14 2018
 
 @author: derekh
 """
+# Part 1 - Data Preprocessing
 
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Import dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data - Do this if categorical data is string
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_text)
+
+#Creating your classifier
+
+#Predicting the test set results
+y_pred = classifer.predict(X_test)
+
+#Making the confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
